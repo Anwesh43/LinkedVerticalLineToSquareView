6affeb70c35b98c0ca385af0a36b5779d663e0a2
Anwesh43 <anweshthecool0@gmail.com>
2019-05-24 09:50:31 +0530

    adding libraries and constants

--- verticallinetosquareview/src/main/java/com/anwesh/uiprojects/verticallinetosquareview/VerticalLineToSquareView.kt
+++ verticallinetosquareview/src/main/java/com/anwesh/uiprojects/verticallinetosquareview/VerticalLineToSquareView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.verticallinetosquareview
 /**
  * Created by anweshmishra on 24/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val squares : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#311B92")
+val backColor : Int = Color.parseColor("#BDBDBD")
